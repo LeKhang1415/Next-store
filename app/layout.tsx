fff446cd73697e5a41c868033b66de7395301961
khang <chaoskid1415@gmail.com>
2025-07-22 12:15:49 +0700

    Navbar Component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Container from "@/components/global/Container";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -14,8 +16,11 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" suppressHydrationWarning>
-            <body className={inter.className}>{children}</body>
+        <html lang="en">
+            <body className={inter.className}>
+                <Navbar />
+                <Container className="py-20">{children}</Container>
+            </body>
         </html>
     );
 }
